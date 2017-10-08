021a420e72038f85c871350abdb07022482c1083
Louis D'hauwe <louisdhauwe@silverfox.be>
2017-10-08 17:10:58 +0200

    Cleanup

--- HandleViewTest/CornerHandleView.swift
+++ HandleViewTest/CornerHandleView.swift
@@ -40,23 +40,15 @@ class CornerHandleView: UIView {
 		}
 	}
 	
-	//	func innerRadius(for outerRadius: CGFloat, with handleWidth: CGFloat) -> CGFloat {
-	//		return outerRadius - handleWidth / 2
-	//	}
-	
-	//	func outerRadius(for innerRadius: CGFloat, with handleWidth: CGFloat) -> CGFloat {
-	//		return innerRadius + handleWidth / 2
-	//	}
-	
 	override func draw(_ rect: CGRect) {
 		
 		guard let context = UIGraphicsGetCurrentContext() else {
 			return
 		}
 		
+		self.tintColor.setFill()
+
 		context.saveGState()
-		
-		//		let outerRadius: CGFloat = self.outerRadius(for: innerRadius, with: handleWidth)
 		
 		let outerRadii = CGSize(width: outerRadius, height: outerRadius)
 		let innerRadii = CGSize(width: innerRadius, height: innerRadius)
@@ -65,44 +57,27 @@ class CornerHandleView: UIView {
 		
 		let outerRoundedRect = UIBezierPath(roundedRect: outerRect, byRoundingCorners: .topLeft, cornerRadii: outerRadii)
 		
-//		let innerRect = CGRect(x: rect.origin.x + handleWidth, y: rect.origin.y + handleWidth, width: rect.width - handleWidth*1.5, height: rect.height - handleWidth*1.5)
-
 		let clipRect = UIBezierPath(rect: CGRect(x: 0, y: 0, width: rect.width - handleWidth/2, height: rect.height - handleWidth/2))
 		
 		clipRect.addClip()
 
 		let innerRect = CGRect(x: rect.origin.x + handleWidth, y: rect.origin.y + handleWidth, width: rect.width*2, height: rect.height*2)
 
-		
 		let innerRoundedRect = UIBezierPath(roundedRect: innerRect, byRoundingCorners: .topLeft, cornerRadii: innerRadii)
-		
 		
 		outerRoundedRect.append(innerRoundedRect)
 		outerRoundedRect.usesEvenOddFillRule = true
 		
 		outerRoundedRect.addClip()
 		
-		
-		self.tintColor.setFill()
-		
-	
 		context.fill(rect)
-		
-//		context.restoreGState()
-//
-//		UIColor.red.setFill()
-//		innerRoundedRect.fill()
 		
 		
 		context.restoreGState()
 		
-		self.tintColor.setFill()
-		
 		context.fillEllipse(in: CGRect(x: 0, y: rect.height - handleWidth, width: handleWidth, height: handleWidth))
 		
 		context.fillEllipse(in: CGRect(x: rect.width - handleWidth, y: 0, width: handleWidth, height: handleWidth))
-		
-		//		context.rotate(by: CGFloat(position) * .pi/2)
 		
 	}
 	
